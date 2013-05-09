7bc5dea4fb0fe423810fc45bbd1690ebd8742a44
Allen Heavey <ista.allen@gmail.com>
2013-05-09 15:35:54 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            lib: {
+                src: ['./lib/*.js','!./lib/jquery.js'],
+                options: {
+                    laxcomma: true
+                    , debug: true
+                    , curly: true // use braces around block always;
+                    , eqnull: true // check variables == null or undifined;
+                    , globals: {
+                      jQuery: true,
+                      console: true,
+                      module: true,
+                      exports: true
+                    }
+                }
+            }
+        }
+        
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    
+    grunt.registerTask('hint', ['jshint:lib']);
+    grunt.registerTask('default', ['jshint']);
+
+};
+
+
+
+
+
